d3670c05b568d336577e3348355c0ce4d9fd048c
Yuri-VN <varaygvn@gmail.com>
2024-06-28 02:55:42 +0300

    add commit

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -52,4 +52,7 @@ def score_game(game_core_v3) -> int:
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
-score_game(game_core_v3)
+
+if __name__ == '__main__':
+    
+    score_game(game_core_v3)
