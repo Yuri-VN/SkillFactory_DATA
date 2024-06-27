ac0874922205757aaaed592cb9ba1089e00cb1d8
Yuri-VN <varaygvn@gmail.com>
2024-06-27 16:52:09 +0300

    add cod

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -33,3 +33,23 @@ def game_core_v3(number: int = 1) -> int:
 
     return count
 #game_core_v3()
+
+def score_game(game_core_v3) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        game_core_v3 ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core_v3(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+score_game(game_core_v3)
