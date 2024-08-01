51efcd217c53f807fc817829f0f49879ffefd18c
Yuri-VN <varaygvn@gmail.com>
2024-08-01 23:55:47 +0300

    edit

--- project_1_Pandas/Введение/Base.py
+++ project_1_Pandas/Введение/Base.py
@@ -1,2 +1,9 @@
 import pandas as pd
 print(pd.__name__)
+
+countries = pd.Series(
+    data = ['Англия', 'Канада', 'США', 'Россия', 'Украина', 'Беларусь', 'Казахстан'],
+    index = ['UK', 'CA', 'US', 'RU', 'UA', 'BY', 'KZ'],
+    name = 'countries'
+)
+print (countries)
