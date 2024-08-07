e317347a53fbf3c92073089cb44262d2a924de50
Yuri-VN <varaygvn@gmail.com>
2024-08-07 13:14:14 +0300

    edit

--- project_1_Pandas/Введение/data_to_csv.py
+++ project_1_Pandas/Введение/data_to_csv.py
@@ -6,5 +6,9 @@ countries_df = pd.DataFrame({
     'area': [133396, 9984670, 9826630, 17125191, 603628, 207600, 2724902]
 })
 
-# print (countries_df)
 countries_df.to_csv('project_1_Pandas/data/countries.csv', index=False, sep=';')
+
+# По ссылке
+# data = pd.read_csv('https://github.com/Yuri-VN/SkillFactory_DATA/blob/main/project_1_Pandas/data/countries.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/esabunor/MLWorkspace/master/melb_data.csv')
+print (data)
