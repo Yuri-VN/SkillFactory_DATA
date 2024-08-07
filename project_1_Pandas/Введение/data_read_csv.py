400f37dda61291cc36e85f1c9465ca8b6ff4191d
Yuri-VN <varaygvn@gmail.com>
2024-08-07 14:29:20 +0300

    edit

--- project_1_Pandas/Введение/data_read_csv.py
+++ project_1_Pandas/Введение/data_read_csv.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
-countries_data = pd.read_csv('project_1_Pandas/data/countries.csv', sep=';')
-print (countries_data)
+# тренировка
+#countries_data = pd.read_csv('project_1_Pandas/data/countries.csv', sep=';')
+#print (countries_data)
+
+
+# практика
+countries_data = pd.read_csv('project_1_Pandas/data/melb_data.csv', sep=',')
+#print (countries_data)
+
