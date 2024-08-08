6d7aac974e4a3996428058c27df372d26ef0a6a5
Yuri-VN <varaygvn@gmail.com>
2024-08-08 14:45:35 +0300

    edit

--- project_1_Pandas/Введение/data_read_csv.py
+++ project_1_Pandas/Введение/data_read_csv.py
@@ -6,8 +6,22 @@ import pandas as pd
 
 # По ссылке
 #data = pd.read_csv('https://raw.githubusercontent.com/esabunor/MLWorkspace/master/melb_data.csv')
+
+# Практика
 melb_data = pd.read_csv('https://raw.githubusercontent.com/Yuri-VN/SkillFactory_DATA/main/project_1_Pandas/data/melb_data.csv')
 print (melb_data)
 
 a = melb_data.iloc[3521, 14] / melb_data.iloc[1690, 14]
 print(round(a))
+
+# первые 5 строк
+print(melb_data.head())
+
+# последние 7 строк
+print (melb_data.tail(7))
+
+# размерность таблицы
+print (melb_data.shape)
+
+# Получение информации о столбцах
+print (melb_data.info())
