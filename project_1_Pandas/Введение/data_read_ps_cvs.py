fd38e791c917a7de4956671110c388f20bc5b86d
Yuri-VN <varaygvn@gmail.com>
2024-08-27 13:04:34 +0300

    edit

--- project_1_Pandas/Введение/data_read_ps_cvs.py
+++ project_1_Pandas/Введение/data_read_ps_cvs.py
@@ -7,3 +7,7 @@ print (melb_data_ps.head())
 # Вариант 2
 #melb_data_ps = pd.read_csv('https://raw.githubusercontent.com/Yuri-VN/SkillFactory_DATA/main/project_1_Pandas/data/melb_data_ps.csv')
 #print (melb_data_ps.head())
+
+# Практика.
+#citi_bike = pd.read_csv('https://raw.githubusercontent.com/Yuri-VN/SkillFactory_DATA/main/project_1_Pandas/data/citibike-tripdata.csv')
+#print(citi_bike.head())
